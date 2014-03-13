92f1f755353bc383082071cabece615b6902b664
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2014-03-13 13:09:40 +0100

    renamed Main -> MainActivity

--- src/com/boombird/epubcoverfinder/MainActivity.java
+++ src/com/boombird/epubcoverfinder/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class Main extends Activity
+public class MainActivity extends Activity
 {
 
 	@Override
